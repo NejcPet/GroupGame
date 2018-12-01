2850734d9e159ad04a0cd28ddf701210927c2532
Nejc Petric <nejc.doha@gmail.com>
2018-12-01 12:58:40 +0100

    Dashing first attempt, jump animation.

---
+++ Best Game/Assets/Scripts/DashScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashScript : MonoBehaviour
+{
+
+    [SerializeField]
+    float dashForce;
+    [SerializeField]
+    float durationVar = 0.1f;
+    float dashDuration = 0.1f;
+    [SerializeField]
+    float varCD;
+    float dashCoolDown = 5f;
+    [SerializeField]
+    bool canDash;
+    Rigidbody2D playerRb;
+    CharacterController2D playerController;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        playerController = GetComponent<CharacterController2D>();
+        varCD = dashCoolDown;
+        durationVar = dashDuration;
+        canDash = true;
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.E) && canDash)
+        {
+            if (playerController.m_FacingRight == true)
+            {
+                playerRb.AddForce(transform.right * dashForce);
+            }
+            else
+            {
+                playerRb.AddForce(-transform.right * dashForce);
+            }
+            durationVar -= Time.deltaTime;
+            if (durationVar < 0)
+            {
+                canDash = false;
+                durationVar = dashDuration;
+            }
+        }
+
+        if (canDash == false)
+        {
+            varCD -= Time.deltaTime;
+            if (varCD <= 0)
+            {
+                canDash = true;
+                varCD = dashCoolDown;
+
+            }
+        }
+    }
+}
